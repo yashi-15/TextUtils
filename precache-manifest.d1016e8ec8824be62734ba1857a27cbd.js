58c94cce526d4c85e1e0a8bc88fbfe9635093162
yashi-15 <sharmayashi270@gmail.com>
2024-03-30 14:51:30 +0530

    Updates

---
+++ precache-manifest.d1016e8ec8824be62734ba1857a27cbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adddf1befc824c54e102cae3e2b1799e",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "2e97e760a00c07d0d1c8",
+    "url": "/TextUtils/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "8946a0edaa5c8cb07476",
+    "url": "/TextUtils/static/js/2.2a068b1b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils/static/js/2.2a068b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/TextUtils/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "2e97e760a00c07d0d1c8",
+    "url": "/TextUtils/static/js/main.ebc3d628.chunk.js"
+  },
+  {
+    "revision": "f41fec5e52ab17bc18cc",
+    "url": "/TextUtils/static/js/runtime-main.f951074a.js"
+  }
+]);
