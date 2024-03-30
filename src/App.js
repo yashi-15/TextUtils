7d5a90008ec4ae2ccb84976193210e833af9aa27
yashi-15 <sharmayashi270@gmail.com>
2024-03-30 16:13:00 +0530

    nav collapse button error solved and deployment process to be done for github pages is done here

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import Alert from "./components/Alert";
-import About from "./components/About";
+// import About from "./components/About";
 // import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {  
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,23 +12,15 @@ export default function Navbar(props) {
         {/* <Link className={`navbar-brand text-${props.mode === "dark" ? "light":"dark"}`} to="/">
           Text-Utils
         </Link> */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="/navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className={`nav-link active text-${props.mode === "dark" ? "light":"dark"}`} aria-current="page" href="/">
+              {/* <a className={`nav-link active text-${props.mode === "dark" ? "light":"dark"}`} aria-current="page" href="/">
                 Home
-              </a>
+              </a> */}
               {/* <Link className={`nav-link active text-${props.mode === "dark" ? "light":"dark"}`} aria-current="page" to="/">
                 Home
               </Link> */}
