5cc5df8b66754afd1ee74ffa56053c051ddb1ac4
yashi-15 <sharmayashi270@gmail.com>
2024-03-31 09:11:15 +0530

    corrected- Word count, button disabling, margin

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -55,7 +55,7 @@ export default function Textform(props) {
   };
   return (
     <>
-      <div className="container m-5">
+      <div className="container my-5">
         <h3>{props.heading}</h3>
         <textarea
           className={`form-control ${props.mode === "dark" ? "text-bg-secondary":"text-bg-light"}`}
@@ -66,6 +66,7 @@ export default function Textform(props) {
         ></textarea>
         <button
           type="button"
+          disabled = {Text.length===0?true:false}
           onClick={handleUpCase}
           className="btn btn-primary m-3"
         >
@@ -74,6 +75,7 @@ export default function Textform(props) {
         </button>
         <button
           type="button"
+          disabled = {Text.length===0?true:false}
           onClick={handleLoCase}
           className="btn btn-primary m-3"
         >
@@ -82,6 +84,7 @@ export default function Textform(props) {
         </button>
         <button
           type="button"
+          disabled = {Text.length===0?true:false}
           onClick={handleAltCase}
           className="btn btn-primary m-3"
         >
@@ -90,6 +93,7 @@ export default function Textform(props) {
         </button>
         <button
           type="button"
+          disabled = {Text.length===0?true:false}
           onClick={handleInverseCase}
           className="btn btn-primary m-3"
         >
@@ -98,6 +102,7 @@ export default function Textform(props) {
         </button>
         <button
           type="button"
+          disabled = {Text.length===0?true:false}
           onClick={handleCopyTxt}
           className="btn btn-primary m-3"
         >
@@ -106,6 +111,7 @@ export default function Textform(props) {
         </button>
         <button
           type="button"
+          disabled = {Text.length===0?true:false}
           onClick={handleClearTxt}
           className="btn btn-primary m-3"
         >
@@ -113,13 +119,14 @@ export default function Textform(props) {
           Clear Text
         </button>
       </div>
-      <div className="container m-5">
+      <div className="container my-5">
         <h3>Your Text Summary Here</h3>
         <p>
           {" "}
-          {Text.split(" ").length} words and {Text.length} letters
+          {Text.split(" ").filter((element)=>{return element.length!==0}).length} words and {Text.length} letters
         </p>
-        <p>Average read time: {0.005 * Text.split(" ").length} minutes.</p>
+        <p>Average read time: {0.005 * Text.split(" ").filter((element)=>{return element.length!==0}).length} minutes.</p>
+        <h4>Preview</h4>
         <p className={`${props.mode === "dark" ? "text-bg-primary":"bg-primary-subtle"} p-3`}> {Text}</p>
       </div>
     </> 
